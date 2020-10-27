5c254df03517e889629f9fa24f096ff390c596c4
Ameer N <ameermahmood98@hotmail.com>
2020-10-27 18:06:39 +0000

    update

---
+++ odin_on_rails/my_first_rails_app/grocery-store-app/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :reviews
+  resources :employees
+  resources :departments
+  resources :stores
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "departments#index"
+end
